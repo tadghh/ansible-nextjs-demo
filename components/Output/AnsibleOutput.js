8e935f8a8b18e40fd44584fa2157e7db66da44fa
tadghh <thenry.he@gmail.com>
2023-07-07 13:24:55 -0500

    Output component

---
+++ components/Output/AnsibleOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AnsibleOutput({ outputData = "" }) {
+    return (
+        <div className='m-4 text-lg text-black border-2 border-black rounded-md grow-0 h-1/6 bg-gray-50'>
+            <h1>Output:</h1>
+            <br></br>
+            {outputData}
+        </div>
+    );
+}
+
--- pages/manage/index.js
+++ pages/manage/index.js
@@ -5,6 +5,7 @@ import { validationSchema } from "@/yup-schemas/userschema";
 import AgentContainer from "@/components/AgentMenu/AgentContainer";
 import { AgentTypes } from "@/AgentsInfo/AgentEnums";
 import { useState } from "react";
+import AnsibleOutput from "@/components/Output/AnsibleOutput";
 
 export default function Manage() {
     const { register, setValue, handleSubmit, formState: { errors } } = useForm({
@@ -23,9 +24,10 @@ export default function Manage() {
             });
             console.log(data)
 
-            let responsedata = await response.json();
+            //  let responsedata = await response.json();
             if (response.status == 200) {
-                setOutputData(response.body)
+                setOutputData(response.json())
+                console.log("no bang")
             } else {
 
                 console.log("Bang!")
@@ -40,7 +42,7 @@ export default function Manage() {
 
     return (
         <div className="flex items-center justify-center h-screen overflow-hidden font-mono text-sm bg-white">
-            <div className='flex-grow w-3/4 m-4 text-lg text-black border-2 border-black rounded-md bg-gray-50 h-3/4'>
+            <div className='flex flex-col flex-grow w-3/4 m-4 text-lg text-black border-2 border-black rounded-md bg-gray-50 h-3/4'>
                 <AgentContainer selectedAgent={setAgentType} setValue={setValue} register={register} />
 
                 <form className="flex flex-col items-center " onSubmit={handleSubmit((data) => onSubmit(data))}>
@@ -57,6 +59,8 @@ export default function Manage() {
 
                     <button className="border-2 border-black" type="submit">Submit</button>
                 </form>
+                <div className="grow" />
+                <AnsibleOutput outputData={outputData} />
             </div>
         </div>
     );
